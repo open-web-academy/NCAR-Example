6d969b33d1daacf82ac2695b207f0ccd94eb3481
Noe Huaccharaque <noemk2@gmail.com>
2022-05-12 22:22:52 -0500

    falta promesas

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,28 @@
-/*  
-    Dia 1 : Interacción con contratos inteligentes.
-  
-    - ☑️ Clona el repositorio de Github, compila y despliega el contrato. 
-    - ☑️Crea la estructura de archivos para tu contrato inteligente, es decir, los archivos migrate.rs, internals.rs, enumerations.rs y los que consideres necesarios.
-    -☑️ Implementa las buenas prácticas recomendadas por el Protocolo de NEAR para el lenguaje de programación Rust.
-    -☑️  Corrige el archivo **Cargo.toml** para optimizar el peso del archivo compilado.
-    - ¡Compila y Despliega tu contrato para realizar las pruebas necesarias y seguir añadiendo las herramientas para escalabilidad y mantenimiento para tu DApp!
-
-*/
-
-
-/*  
-    Estructura de archivos de un contrato:
-
-    lib.rs: estructura de contrato y metodos de inicializacion
-
-*/
+use std::borrow::Borrow;
 
 // use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 // use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
-use near_sdk::collections::{ LookupMap };
+use near_sdk::collections::LookupMap;
+use near_sdk::env::log_str;
 // use near_sdk::json_types::{Base64VecU8, U128};
 use near_sdk::serde::{Deserialize, Serialize};
-// use near_sdk::{
-//     env, log, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
-//     PromiseResult, Gas, require, serde_json::json
-// };
 use near_sdk::{
-   near_bindgen, AccountId,  PanicOnDefault 
+    env, log, near_bindgen, require, serde_json::json, AccountId, Balance, CryptoHash, Gas,
+    PanicOnDefault, Promise, PromiseOrValue, PromiseResult,
 };
 
+// use near_sdk::{bindgen, AccountId, PanicOnDefault};
 
-#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Clone)] 
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Item {
     address: String,
     name: String,
     price: u64,
     stock: u64,
-    enabled: bool
+    cid: String,
+    enabled: bool,
 }
 
 #[near_bindgen]
@@ -53,15 +36,13 @@ pub struct Contract {
 impl Contract {
     #[init]
     pub fn init_contract(owner_id: AccountId) -> Self {
-        //calls the other function "new: with some default metadata and the owner_id passed in 
-        Self::new(
-            owner_id
-        )
+        //calls the other function "new: with some default metadata and the owner_id passed in
+        Self::new(owner_id)
     }
 
     #[init]
     pub fn new(owner_id: AccountId) -> Self {
-        //create a variable of type Self with all the fields initialized. 
+        //create a variable of type Self with all the fields initialized.
         let this = Self {
             owner_id,
             records: LookupMap::new(b"a".to_vec()),
@@ -72,34 +53,74 @@ impl Contract {
     }
 
     // Guardar producto
-    pub fn set_products(&mut self, address:String, name:String, price:u64, stock:u64) -> Item {
+    pub fn set_products(
+        &mut self,
+        address: String,
+        name: String,
+        price: u64,
+        stock: u64,
+        cid: String,
+    ) -> Item {
         //validate sender has permition of ROLE_SET_PRODUCT
         let item = Item {
-            address : address.to_string(),
-            name : name.to_string(),
-            price : price,
-            stock : stock,
-            enabled : true
+            address: address.to_string(),
+            name: name.to_string(),
+            price: price,
+            stock: stock,
+            cid: cid.to_string(),
+            enabled: true,
         };
 
-        self.records.insert(&address, &item);
+        log_str(
+            &json!({
+            "address": item.address.clone(),
+            "name": item.name.clone(),
+            "price": item.price.clone(),
+            "stock": item.stock.clone(),
+            "cid": item.cid.clone(),
+            "enabled": item.enabled.clone()
+            })
+            .to_string(),
+        );
 
+        self.records.insert(&address, &item);
         item
     }
 
     // Eliminar producto
-    pub fn delete_products(&mut self, address:String) {
+    pub fn delete_products(&mut self, address: String) {
         //validate sender has permition of ROLE_DELETE_PRODUCT
-       
+
         // Use env::log to record logs permanently to the blockchain!
-        let _delete_product = self.records.get(&address); // none or some
+        let delete_product = match self.records.get(&address) {
+            Some(item) => item.clone(),
+            None => Item {
+                address: "".to_string(),
+                name: "".to_string(),
+                price: 0,
+                stock: 0,
+                cid: "".to_string(),
+                enabled: false,
+            },
+        };
+
+        log_str(
+            &json!({
+            "address": delete_product.address.clone(),
+            "name": delete_product.name.clone(),
+            "price": delete_product.price.clone(),
+            "stock": delete_product.stock.clone(),
+            "cid": delete_product.cid.clone(),
+            "enabled": delete_product.enabled.clone()
+            })
+            .to_string(),
+        );
 
         self.records.remove(&address);
-   }
+    }
 
     // Consultar producto
-    pub fn get_products(&self, address:String) -> Option<Item>{
+    pub fn get_products(&self, address: String) -> Option<Item> {
         self.records.get(&address)
-   }
-
+    }
 }
