75b2c8ce520fd5c268d5ae8e01054f96272eeb62
Noe Huaccharaque <noemk2@gmail.com>
2022-05-13 02:35:07 -0500

    no dao

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::borrow::Borrow;
+// use std::borrow::Borrow;
 
 // use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
@@ -12,7 +12,9 @@ use near_sdk::{
     PanicOnDefault, Promise, PromiseOrValue, PromiseResult,
 };
 
+// use near_sdk::Gas;
 // use near_sdk::{bindgen, AccountId, PanicOnDefault};
+pub const TGAS: u64 = 10_000_000_000_000;
 
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
@@ -125,4 +127,45 @@ impl Contract {
     }
 
     // Cross Contract Callbacks hight level
+
+    //dao
+
+    #[cfg(target_arch = "wasm32")]
+    pub fn upgrade(self) {
+        use near_sys as sys;
+        assert!(env::predecessor_account_id() == self.owner_id);
+        //input is code:<Vec<u8> on REGISTER 0
+        //log!("bytes.length {}", code.unwrap().len());
+        const GAS_FOR_UPGRADE: u64 = 20 * TGAS; //gas occupied by this fn
+                                                //const BLOCKCHAIN_INTERFACE_NOT_SET_ERR: &str = "Blockchain interface not set.";
+                                                //after upgrade we call *pub fn migrate()* on the NEW CODE
+        let current_id = env::current_account_id();
+        let migrate_method_name = "migrate".as_bytes().to_vec();
+        let attached_gas = env::prepaid_gas() - env::used_gas() - Gas(GAS_FOR_UPGRADE);
+        unsafe {
+            // Load input (new contract code) into register 0
+            sys::input(0);
+
+            //prepare self-call promise
+            let promise_id = sys::promise_batch_create(
+                current_id.as_bytes().len() as _,
+                current_id.as_bytes().as_ptr() as _,
+            );
+
+            //1st action, deploy/upgrade code (takes code from register 0)
+            sys::promise_batch_action_deploy_contract(promise_id, u64::MAX as _, 0);
+
+            // 2nd action, schedule a call to "migrate()".
+            // Will execute on the **new code**
+            sys::promise_batch_action_function_call(
+                promise_id,
+                migrate_method_name.len() as _,
+                migrate_method_name.as_ptr() as _,
+                0 as _,
+                0 as _,
+                0 as _,
+                u64::from(attached_gas),
+            );
+        }
+    }
 }
